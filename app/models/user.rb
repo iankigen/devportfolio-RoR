a067b318ebbab2513c5454231d44800f1a154073
Ian Kigen <ian.gabe.ian@gmail.com>
2018-06-07 17:39:46 +0300

    add name virtual attrs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates_presence_of :name
+
+  def first_name
+    name.split.first
+  end
+
+  def last_name
+    @name = name.split.last
+  end
 end

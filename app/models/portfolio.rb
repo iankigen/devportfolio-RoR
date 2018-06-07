07816e1da04ddc8efae682fc6d39dc159b3f4153
Ian Kigen <ian.gabe.ian@gmail.com>
2018-06-07 10:15:21 +0300

    add custom scope for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,11 @@
 class Portfolio < ApplicationRecord
   validates_presence_of :title, :subtitle, :body, :main_image, :thumb_image
+
+  def self.angular
+    where(
+      subtitle: 'Angular'
+    )
+  end
+
+  scope :ruby_on_rails, -> { where(subtitle: 'Ruby on Rails') }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+3.times do |topic|
+  Topic.create(
+    title: "Topic #{topic}"
+  )
+end
+
 10.times do |blog|
   Blog.create!(
     title: "Blog Post #{blog}",
@@ -15,7 +22,8 @@ tempor incididunt ut labore et
 dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
 laboris nisi ut aliquip ex ea commodo onsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
 pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
-laborum.'
+laborum.',
+    topic_id: Topic.last.id
   )
 end
 skills = %w[Python Django Flask Rails Javascript]
@@ -26,10 +34,25 @@ skills = %w[Python Django Flask Rails Javascript]
   )
 end
 
-9.times do |portfolio|
+6.times do |portfolio|
   Portfolio.create(
     title: "Portfolio #{portfolio}",
-    subtitle: "Portfolio #{portfolio}",
+    subtitle: 'Ruby on Rails',
+    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+tempor incididunt ut labore et
+dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+laboris nisi ut aliquip ex ea commodo onsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+laborum.',
+    main_image: 'https://via.placeholder.com/600x400',
+    thumb_image: 'https://via.placeholder.com/350x200'
+  )
+end
+
+3.times do |portfolio|
+  Portfolio.create(
+    title: "Portfolio #{portfolio}",
+    subtitle: 'Angular',
     body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
 tempor incididunt ut labore et
 dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco

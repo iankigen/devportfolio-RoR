4044d4549dadff917f5332c4901360523799346a
Ian Kigen <ian.gabe.ian@gmail.com>
2018-06-12 11:07:28 +0300

    - add collection based partials

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,8 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require 'vendor/jquery.min.js'
+//= require 'vendor/bootstrap.min.js'
 //= require activestorage
 //= require turbolinks
 //= require_tree .

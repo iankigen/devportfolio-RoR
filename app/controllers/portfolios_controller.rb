ac1c4effa4242a978e97499e1792de00592b8454
Ian Kigen <ian.gabe.ian@gmail.com>
2018-06-12 11:07:28 +0300

    - add collection based partials

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,7 +2,7 @@
 
 class PortfoliosController < ApplicationController
   before_action :set_portfolio, only: [:edit, :update, :show, :destroy]
-  layout 'portfolio'
+  # layout 'portfolio'
 
   def index
     @portfolio_items = Portfolio.all
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
   def login_helper
     if current_user.is_a?(User)
-      link_to "Logout", destroy_user_session_path, method: :delete
+      "<li> #{link_to "Logout", destroy_user_session_path, method: :delete} </li>".html_safe
     else
-      "#{link_to 'Login', new_user_session_path} |
-      #{link_to 'Register', new_user_registration_path}".html_safe
+      "<li>#{link_to 'Login', new_user_session_path}</li>
+      <li>#{link_to 'Register', new_user_registration_path}</li>".html_safe
     end
   end
 

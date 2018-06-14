f0d8452fd2101a1579e53eb7b56835daf58ca21d
Ian Kigen <ian.gabe.ian@gmail.com>
2018-06-14 21:56:01 +0300

    refactor nav to helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,18 @@
 module ApplicationHelper
   def login_helper(style = 'nav-link')
+    login = style
+    register = style
+    if active?(new_user_session_path)
+      login += ' active'
+    end
+    if active?(new_user_registration_path)
+      register += ' active'
+    end
     if current_user.is_a?(GuestUser)
-      "#{link_to 'Login', new_user_session_path, class: style}
-      #{link_to 'Register', new_user_registration_path, class: style}".html_safe
+      "#{link_to 'Login', new_user_session_path, class: login}
+      #{link_to 'Register', new_user_registration_path, class: register}".html_safe
     else
-      "#{link_to 'Logout', destroy_user_session_path, method: :delete, class: style}".html_safe
+      (link_to 'Logout', destroy_user_session_path, method: :delete, class: style).to_s.html_safe
     end
   end
 
@@ -18,5 +26,43 @@ module ApplicationHelper
 
   def copyright
     "&copy; #{Time.now.year} | <b>Ian Kigen</b> All rights reserved".html_safe
+  end
+
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: 'Home'
+      },
+      {
+        url: blogs_path,
+        title: 'Blog'
+      },
+      {
+        url: portfolios_path,
+        title: 'Portfolio'
+      },
+      {
+        url: contact_path,
+        title: 'Contact'
+      },
+      {
+        url: about_path,
+        title: 'About Me'
+      }
+    ]
+  end
+
+  def nav_helper(style)
+    nav_links = ''
+
+    nav_items.each do |item|
+      nav_links << "<a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a>"
+    end
+    nav_links.html_safe
+  end
+
+  def active?(path)
+    'active' if current_page? path
   end
 end

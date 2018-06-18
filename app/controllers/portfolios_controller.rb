9b4272528b0a0b18ba9388956d1cf8b2ba4e66f0
Ian Kigen <ian.gabe.ian@gmail.com>
2018-06-18 11:35:08 +0300

    order portfolio by position

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -6,7 +6,7 @@ class PortfoliosController < ApplicationController
   layout 'portfolio'
 
   def index
-    @portfolio_items = Portfolio.all
+    @portfolio_items = Portfolio.by_position
     @page_title += " | #{params[:controller]}"
   end
 
